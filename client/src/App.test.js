7b87e285e7e3b3d1a4f7f1122c17b4891f68a003
Your Name <you@example.com>
2021-08-03 14:39:15 +0100

    tt

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,7 +4,7 @@ import App from "./App";
 describe("The App Component", () => {
   test("it renders", async () => {
     render(<App />);
-    const headerElement = await screen.findByText(/Todos Full-Stack App/i);
+    const headerElement = await screen.findByText(/sdfsdfsdfdsfds/i);
     expect(headerElement).toBeInTheDocument();
   });
 });
